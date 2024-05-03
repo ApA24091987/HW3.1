105b97382226b3eb0d2a4ef4c5a591fd6afb7dfe
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-04 01:02:34 +0300

    Update yuri2.py

--- yuri2.py
+++ yuri2.py
@@ -2,11 +2,11 @@ name="Yuri"
 print(name, type(name))
 age=36
 print(age, type(age))
-new_age=37
-print(new_age, type(new_age))
+age=age+1
+print(age, type(age))
 is_student='True'
 print(is_student, type('True'))
 print('name:Yuri')
 print('age:36')
-print('new_age:37')
+print('new_age:',age)
 print('is_student:True')
